4632fbe65b3d61ed5d79b13e05329ce5efb3fc3d
CarlosHuerta <charlyhg9597@gmail.com>
2019-05-14 11:05:01 +0300

    update

--- ClassWorkWeek13_Octave.m
+++ ClassWorkWeek13_Octave.m
@@ -1,6 +1,6 @@
 Q = [-12 12; 3 -3];
 index=1;
-for t=0:0.1:10
+for t=0:0.01:10
   P = expm(Q*t);
   ROW1_1(index) = P(1,1);
   ROW1_2(index) = P(1,2);
@@ -8,5 +8,8 @@ for t=0:0.1:10
   ROW2_2(index) = P(2,2);
   index = index +1;
 endfor
-t = 0:0.1:10;
-plot(t,ROW1_1,'b',t,ROW1_2,'b',t,ROW2_1,'r',t,ROW2_2,'r');
+t = 0:0.01:10;
+figure(1);
+plot(t,ROW1_1,'b',t,ROW1_2,'r',t,ROW2_1,'y',t,ROW2_2,'m');
+figure(2);
+semilogx(t,ROW1_1,'b',t,ROW1_2,'r',t,ROW2_1,'y',t,ROW2_2,'m');
--- ClassWorkWeek13_R/ClassWorkWeek13_R_Script.R
+++ ClassWorkWeek13_R/ClassWorkWeek13_R_Script.R
@@ -5,7 +5,7 @@ ROW1_1 <-  matrix(c(0:100),nrow=1,ncol=101,byrow=TRUE)
 ROW1_2 <-  matrix(c(0:100),nrow=1,ncol=101,byrow=TRUE)
 ROW2_1 <-  matrix(c(0:100),nrow=1,ncol=101,byrow=TRUE)
 ROW2_2 <-  matrix(c(0:100),nrow=1,ncol=101,byrow=TRUE)
-for (t in seq(0,10,by=0.1))
+for (t in seq(0,10,by=0.01))
 {
   P = expm(Q*t);
   ROW1_1[index] = P[c(1),c(1)];
@@ -14,12 +14,21 @@ for (t in seq(0,10,by=0.1))
   ROW2_2[index] = P[c(2),c(2)];
   index = index +1;
 }
-t <- seq(0,10,by=0.1)
+t <- seq(0,10,by=0.01)
 plot(t,ROW1_1,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="blue")
 par(new=T)
-plot(t,ROW1_2,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="blue")
+plot(t,ROW1_2,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="red")
 par(new=T)
-plot(t,ROW2_1,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="red")
+plot(t,ROW2_1,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="yellow")
 par(new=T)
-plot(t,ROW2_2,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="red")
+plot(t,ROW2_2,type="l",xlim=c(0.0,10),ylim=c(0.0,1),col="purple")
+par(new=F)
+
+plot(t,ROW1_1,log="x",pch=16,type="l",lwd=2,ylim=c(0.0,1),col="blue")
+par(new=T)
+plot(t,ROW1_2,log="x",pch=16,type="l",lwd=2,ylim=c(0.0,1),col="red")
+par(new=T)
+plot(t,ROW2_1,log="x",pch=16,type="l",lwd=2,ylim=c(0.0,1),col="yellow")
+par(new=T)
+plot(t,ROW2_2,log="x",pch=16,type="l",lwd=2,ylim=c(0.0,1),col="purple")
 par(new=F)
